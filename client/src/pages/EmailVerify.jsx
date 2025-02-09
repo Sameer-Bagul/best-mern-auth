157474ca47c74c543e03b501b87413e04006d321
sameer <sameerbagul2004@gmail.com>
2025-02-09 19:42:54 +0530

    existing repo is public now

---
+++ client/src/pages/EmailVerify.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div>Email Verify Page</div>
+  )
+}
+
+export default EmailVerify
---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ResetPassword = () => {
+  return (
+    <div>Reset Password Page</div>
+  )
+}
+
+export default ResetPassword
